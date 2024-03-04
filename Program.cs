d7a54d338007e212fec58c9b66c1597698d45291
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-04 22:59:07 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Result = Calculator.Equal("AB","AB");
+            if(Result)
+            {
+                Console.WriteLine("Vale Match");
+            }
+            else { 
+                Console.WriteLine("Value not matche");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Calculator
+    {
+        public static bool Equal(string value1, string value2)
+        {
+            return value1 == value2;
+        }
+    }
+}
